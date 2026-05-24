5ff534fcbb2c07a9a0f49366d3b820d366db7b04
jir <jir@gmail.com>
2026-05-23 20:20:55 -0500

    first commit cars_project

---
+++ cars_project/src/main/java/com/mycompany/cars_project/Cars_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cars_project;
+
+/**
+ *
+ * @author Jirley Gissel Suarez
+ */
+public class Cars_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
